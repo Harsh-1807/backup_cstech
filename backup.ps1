ba938031b62859757c9627f6e8172544b806f9cc
Harsh Bachhav <nkharshbachhav@gmail.com>
2026-07-08 11:28:00 +0530

    Backup 2026-07-08_11-28

--- backup.ps1
+++ backup.ps1
@@ -1,5 +1,9 @@
 $git="$env:LOCALAPPDATA\GitHubDesktop\app-3.6.2\resources\app\git\cmd\git.exe"
 
+Set-Location "C:\Users\13519\LULC"
+
 & $git add .
-& $git commit -m "Backup $(Get-Date -Format yyyy-MM-dd)"
+
+& $git commit -m "Backup $(Get-Date -Format yyyy-MM-dd_HH-mm)"
+
 & $git push origin backup
